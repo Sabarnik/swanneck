da9256e268c83ac63c16511b85423265c8f3cd9d
Sabarnik <lahiri.sabarnik47@gmail.com>
2026-02-23 13:31:28 +0530

    Fix paths and config

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,21 @@ import { fileURLToPath } from 'node:url';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
-export default defineConfig({
-    base: '/swanneck/',
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                about: resolve(__dirname, 'about.html'),
-                products: resolve(__dirname, 'products.html'),
-                industries: resolve(__dirname, 'industries.html'),
-                sustainability: resolve(__dirname, 'sustainability.html'),
-                whySwanneck: resolve(__dirname, 'why-swanneck.html'),
-                contact: resolve(__dirname, 'contact.html'),
+export default defineConfig(({ command }) => {
+    return {
+        base: command === 'build' ? '/swanneck/' : '/',
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    about: resolve(__dirname, 'about.html'),
+                    products: resolve(__dirname, 'products.html'),
+                    industries: resolve(__dirname, 'industries.html'),
+                    sustainability: resolve(__dirname, 'sustainability.html'),
+                    whySwanneck: resolve(__dirname, 'why-swanneck.html'),
+                    contact: resolve(__dirname, 'contact.html'),
+                },
             },
         },
-    },
+    }
 });
